ff006403a312dd4a559adf4cd9227a22e041339e
Juan24rangel <juan2224658@correo.uis.edu.co>
2022-09-29 10:00:13 -0500

    Ejercicio

---
+++ repetitiva.py
@@ -0,0 +1,12 @@
+# Instrucciones repetitivas
+
+#input
+N = int(input("Digite el valor:"))
+# processing
+sum = 0
+i = 1
+while i<=N:
+    sum = sum + i
+    i = i+1   
+# output
+print("Valor: " +str(sum))
